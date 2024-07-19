f8eb73424bf4afd82f9eb79a642ed5e6db9ad7d6
Pedro Miranda <pedroemanuelsm0@outlook.com>
2024-07-19 04:51:57 -0300

    feat (application, impl): delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -10,6 +11,7 @@ import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) { 
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.creatSellerDao();
 		
@@ -41,8 +43,13 @@ public class Program {
 		sellerDao.update(seller);
 		System.out.println("Update Completed");
 		
+		System.out.println("\n#### TEST 6: seller delete ####");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete Completed");
 		
-		
+		sc.close();
 	}
 
 }
